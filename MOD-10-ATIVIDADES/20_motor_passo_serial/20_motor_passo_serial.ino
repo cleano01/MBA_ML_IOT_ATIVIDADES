b02407c0613a0af5dc7322fc41e7d8d8b60cd416
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-09-13 00:19:11 -0300

    (MOD-10) motor serial passo

--- MOD-10-ATIVIDADES/20_motor_passo_serial/20_motor_passo_serial.ino
+++ MOD-10-ATIVIDADES/20_motor_passo_serial/20_motor_passo_serial.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+const int botao1=9, botao2=10; 
 
+void setup() {
+	pinMode(botao1, INPUT);
+	pinMode(botao2, INPUT);
+	
+	//incializando as portas 2 a 5
+	for(int p = 2; p <= 5; p++){
+		pinMode(p, OUTPUT);
+		digitalWrite(p, LOW);
+	}
+	
+	
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	// faz esse sentindo o motor >>>>>>>>
+	if(digitalRead(botao1) == 1 && digitalRead(botao2) == 0){
+	
+		for(int p=2; p <=5; p++){
+		 digitalWrite(p,HIGH);
+		 delay(50);
+		 digitalWrite(p,LOW);
+		}
+	}
+	
+	//faz esse sentindo  motor <<<<<<<<<<
+	if(digitalRead(botao1) == 0 && digitalRead(botao2) == 1){
+	
+	for(int p=5;p>=2;p--){
+		 digitalWrite(p,HIGH);
+		 delay(50);
+		 digitalWrite(p,LOW);
+		}
+	}
 
+		
+	
 }
